87857db443f7c26eaced130c76a89ae46092f1c9
Your Name <zo.ekhtiyari.ze@gmail.com>
2020-03-05 10:56:49 +0330

    const post is add

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
 app.get('/' , (req, res) => {
     res.send('Hello');
 });
 
-app.listen(3000, () => {
-  console.log('server is runing on port 3000')
+app.listen(port, () => {
+  console.log(`server is runing on port ${port}`)
 })
